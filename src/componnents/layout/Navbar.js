a6e32262f996345f5801d9f3d94c5b52870cec33
hareesh43 <hareeshgummani@gmail.com>
2020-04-14 18:02:46 +0530

    Initial setup and Navbar is created

---
+++ src/componnents/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import{Link} from 'react-router-dom';
+
+const Navbar = ()=>
+{
+  return(
+  <nav className = "nav-wrapper grey darken-3">
+
+      <div className = "container">
+        <Link to  = '/' className = "brand-logo">
+            H-Plan
+        </Link>
+
+
+      </div>
+  </nav>
+  )
+}
+
+export default Navbar;
+
+
